1e7443def2d99cd9594176aa992e7e4a3b06de95
Ahshan-Haquc <ahshanulhaqucgub@gmail.com>
2026-06-02 12:27:08 +0600

    Implemented total play count api

--- src/controllers/quizController.ts
+++ src/controllers/quizController.ts
@@ -69,3 +69,32 @@ export const updateHighestScore = async (req: Request, res: Response) => {
     });
 
 }
+
+
+export const updateTotalPlayCount = async (req: Request, res: Response) => {
+    const { quizName } = req.params;
+    if (!quizName) {
+        throw new ApiError(400, 'Quiz name is required.');
+    }
+
+    let convertedName = '';
+    for(let i=0;i<quizName.length;i++){
+        if(i===0){
+            convertedName += quizName[i].toUpperCase();
+        }else{
+            convertedName += quizName[i];
+        }
+    }
+    const quizNameField = `total${convertedName}GamePlayed`;
+
+    await QuizTracking.findOneAndUpdate(
+        {},
+        { $inc: { [quizNameField]: 1 } },
+        { new: true }
+    );
+    res.status(200).json({
+        status: 'success',
+        message: 'Total play count updated successfully.',
+    });
+}
+
