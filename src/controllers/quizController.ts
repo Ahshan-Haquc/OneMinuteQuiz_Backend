4606f50ba011833db257ce1bf471b43d3653e4cd
Ahshan-Haquc <ahshanulhaqucgub@gmail.com>
2026-06-02 12:26:29 +0600

    Implemented update highest scrore api

--- src/controllers/quizController.ts
+++ src/controllers/quizController.ts
@@ -1,36 +1,71 @@
 import { NextFunction, Request, Response } from "express";
+import QuizTracking from "../models/quizTrackingSchema";
+import { ApiError } from "../utils/ApiError";
 
-export const getLandingPageData = async (req: Request, res: Response, next: NextFunction) => {
+export const getLandingPageData = async (req: Request, res: Response) => {
     try {
-        res.status(200).json({ status: 'success', message: 'Landing page data retrieved successfully.' });
+        const existingData = await QuizTracking.findOne();
+
+        if (!existingData) {
+            await QuizTracking.create({});
+            console.log("Quiz tracking document created.");
+        }
+
+        const quizData = await QuizTracking.findOne();
+        res.status(200).json({
+            status: 'success',
+            message: 'Landing page data retrieved successfully.',
+            data: quizData
+        });
     } catch (error) {
-        next(error);
+        res.status(500).json({ status: 'error', message: 'Internal server error.' });
     }
 }
 
 export const getTopThreeHighestScores = async (req: Request, res: Response) => {
-    // const { quizName } = req.params;
-    // if(!quizName){
-    //     throw new ApiError(400, 'Quiz name is required.');
-    // }
-    // const topScores = await Quiz.find({ quizName }).sort({ score: -1 }).limit(3);
-    // res.status(200).json({ status: 'success', data: { topScores } });
+
 }
 
 export const updateHighestScore = async (req: Request, res: Response) => {
-    // const { quizName } = req.params;
-    // if(!quizName){
-    //     throw new ApiError(400, 'Quiz name is required.');
-    // }
-    // const topScores = await Quiz.find({ quizName }).sort({ score: -1 }).limit(3);
-    // res.status(200).json({ status: 'success', data: { topScores } });
-}
+    const { quizName } = req.params;
+    const { score } = req.body;
+    if (!quizName) {
+        throw new ApiError(400, 'Quiz name is required.');
+    }
+    if (!score) {
+        throw new ApiError(400, 'Score is required.');
+    }
 
-export const updateTotalPlayCount = async (req: Request, res: Response) => {
-    // const { quizName } = req.params;
-    // if(!quizName){
-    //     throw new ApiError(400, 'Quiz name is required.');
-    // }
-    // const topScores = await Quiz.find({ quizName }).sort({ score: -1 }).limit(3);
-    // res.status(200).json({ status: 'success', data: { topScores } });
+    const quiz = await QuizTracking.findOne();
+    if (!quiz) {
+        throw new ApiError(404, 'Quiz not found.');
+    }
+
+    const previousTopThreeScores = quiz.get(`${quizName}TopThreeScores`) || { first: 0, second: 0, third: 0 };
+
+    let newTopThreeScores = previousTopThreeScores;
+    if (score > previousTopThreeScores.first) {
+        newTopThreeScores.third = previousTopThreeScores.second;
+        newTopThreeScores.second = previousTopThreeScores.first;
+        newTopThreeScores.first = score;
+    } else if (score > previousTopThreeScores.second) {
+        newTopThreeScores.third = previousTopThreeScores.second;
+        newTopThreeScores.second = score;
+    } else if (score > previousTopThreeScores.third) {
+        newTopThreeScores.third = score;
+    }
+
+    const fieldName = `${quizName}TopThreeScores`;
+
+    const data = await QuizTracking.findOneAndUpdate(
+        {},
+        { $set: { [fieldName]: newTopThreeScores } },
+        { new: true }
+    );
+
+    res.status(200).json({
+        status: 'success',
+        message: 'Scores updated successfully.',
+    });
+
 }
--- src/routes/quizRoutes.ts
+++ src/routes/quizRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { getLandingPageData, getTopThreeHighestScores, updateHighestScore, updateTotalPlayCount } from '../controllers/quizController';
+import { getLandingPageData, getTopThreeHighestScores, updateHighestScore, updateTotalPlayCount, updateRating } from '../controllers/quizController';
+import { catchAsync } from '../utils/catchAsync';
 const router = Router();
 
 router.get('/getLandingPageData', getLandingPageData)
 router.get('/getTopThreeHighestScores/:quizName', getTopThreeHighestScores)
-router.post('/updateHighestScore/:quizName', updateHighestScore)
-router.post('/updateTotalPlayCount/:quizName', updateTotalPlayCount)
+router.patch('/updateHighestScore/:quizName', updateHighestScore)
+router.patch('/updateTotalPlayCount/:quizName', catchAsync(updateTotalPlayCount))
+router.patch('/updateRating/:quizName', catchAsync(updateRating))
 
 export default router;
