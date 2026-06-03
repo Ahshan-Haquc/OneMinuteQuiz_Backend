52d54348fb73d8c78e57a509bbecc3ec774bc7a1
Ahshan-Haquc <ahshanulhaqucgub@gmail.com>
2026-06-03 11:03:56 +0600

    Updated auth controller and feedback routes

--- src/routes/feedbackRoutes.ts
+++ src/routes/feedbackRoutes.ts
@@ -1,6 +1,7 @@
 import { Request, Router } from 'express';
 import { catchAsync } from '../utils/catchAsync';
 import { deleteAllFeedback, deleteFeedbackById, getAllFeedback, sentFeedback } from '../controllers/feedbackController';
+import { userAccessPermission } from '../middlewares/userAccessPermission';
 
 type AuthRequest = Request & {
   userInfo?: import('../models/userSchema').UserDocument;
@@ -8,12 +9,12 @@ type AuthRequest = Request & {
 
 const router = Router();
 
-router.get('/getAllFeedback/:page', catchAsync(getAllFeedback));
+router.get('/getAllFeedback/:page', userAccessPermission, catchAsync(getAllFeedback));
 
 router.post('/sentFeedback', catchAsync(sentFeedback));
 
-router.delete('/deleteFeedbackById/:feedbackId', catchAsync(deleteFeedbackById));
+router.delete('/deleteFeedbackById/:feedbackId', userAccessPermission, catchAsync(deleteFeedbackById));
 
-router.delete('/deleteAllFeedback', catchAsync(deleteAllFeedback));
+router.delete('/deleteAllFeedback', userAccessPermission, catchAsync(deleteAllFeedback));
 
 export default router;
